ff35f9a7707d417a11631386bab26d6baec6cff9
behzad <90694618+behzadiv@users.noreply.github.com>
2023-08-15 11:08:56 +0330

    feat : install axios & get data from API

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
 import { allCharacters } from "../data/data";
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
   const [characters, setCharacters] = useState(allCharacters);
+
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then(({ data }) => setCharacters(data.results.slice(0, 5)))
+      .catch((err) => {
+        return setCharacters([]);
+      });
+  }, []);
+
   return (
     <div className="app">
       <Navbar numOfCharacters={characters.length} />
